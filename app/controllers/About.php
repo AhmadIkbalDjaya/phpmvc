1e51def12b1b8b32a761c14cb09d43ed41d62167
Ahmad Ikbal Djaya <ikbaldjaya@gmail.com>
2022-05-07 14:10:08 +0800

    4-Controller MVC: Membuat controller dan mvc; memanggil halaman sesuai cotroller dan method di url; mengambil parameter dari url

---
+++ app/controllers/About.php
@@ -0,0 +1,10 @@
+<?php
+
+class About{
+    public function index($nama="Ahmad Ikbal", $pekerjaan="Mahasiswa"){
+        echo "halo, nama saya $nama, saya adalah seorang $pekerjaan";
+    }
+    public function page(){
+        echo 'About/page';
+    }
+}
--- app/core/App.php
+++ app/core/App.php
@@ -1,9 +1,46 @@
 <?php
 
 class App {
+    // properti untuk menentukan controller dan method default
+    protected $controller = 'Home';
+    protected $method = 'index';
+    protected $params = [];
+
     public function __construct(){
         $url = $this->parseURL();
-        var_dump($url);
+
+        // controller
+        // cek apakah controller yg di parameter pertama di url ada atau tidak
+        // apakah filenya ada
+        // mengambil controller
+        if(!empty($url)){
+            if( file_exists('../app/controllers/' . $url[0]  . '.php') ){
+                // jika ada maka tinpa property controller
+                $this->controller = $url[0];
+                // hilangkan controller dari elemen arraynya
+                unset($url[0]);
+            }
+        }
+        
+        require_once '../app/controllers/' . $this->controller . '.php';
+        $this->controller = new $this->controller;
+    
+        //method
+        if ( isset($url[1]) ){
+            if( method_exists($this->controller, $url[1]) ){
+                $this->method = $url[1];
+                unset($url[1]);
+            }
+        }
+
+        // params
+        if( !empty($url) ){
+            $this->params = array_values($url);
+        }
+
+        //  jalankan controlle& method, serta kirimkan params jika ada
+        call_user_func_array([$this->controller, $this->method], $this->params);
+    
     }
 
     public function parseURL(){
