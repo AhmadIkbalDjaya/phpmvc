aea3030cbf52e80c4a2784f86b8df7cf79cd1973
Ahmad Ikbal Djaya <ikbaldjaya@gmail.com>
2022-05-07 11:00:55 +0800

    3-Routing MVC: Membuat url menjadi rapi; mengambil url dan memasukkanya di varibel array menggunakan .htaccess

--- app/core/App.php
+++ app/core/App.php
@@ -2,6 +2,19 @@
 
 class App {
     public function __construct(){
-        echo "OK";
+        $url = $this->parseURL();
+        var_dump($url);
+    }
+
+    public function parseURL(){
+        if( isset($_GET['url'])){
+            // rtrim() untuk menghapus karakter tertentu
+            $url = rtrim($_GET['url'], '/');
+            // membersihkan url dari karakter aneh
+            $url = filter_var($url, FILTER_SANITIZE_URL);
+            // memecah url dengan delimiter / dan memasukkan ke dalam elemen array
+            $url = explode('/', $url);
+            return $url;
+        }
     }
 }
