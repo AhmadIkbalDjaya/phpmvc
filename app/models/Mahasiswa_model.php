592c2a3eaaa655baf9f033dd04950d3dd035e51b
Ahmad Ikbal Djaya <ikbaldjaya@gmail.com>
2022-05-08 11:42:02 +0800

    7-Mode.2: mengambil data dari database di file mahasiswa_model.php

--- app/models/Mahasiswa_model.php
+++ app/models/Mahasiswa_model.php
@@ -1,22 +1,29 @@
 <?php
 
 class Mahasiswa_model{
-    private $mhs = [
-        [
-            "nama" => "Ahmad Ikbal Djaya",
-            "nis" => "17091608",
-            "email" => "ikbaldjaya@gmail.com",
-            "alamat" => "Misten"
-        ],
-        [
-            "nama" => "Ahmad Syidqi Ramadan",
-            "nis" => "17091609",
-            "email" => "Syidqi11@gmail.com",
-            "alamat" => "Batangase"
-        ]
-    ];
+    // connect db dengan driver pdo(php data object)
+    // lebih muda dari mysqli
+    private $dbh;   //dbh(database handler) untk menampung koneksi ke db
+    private $stmt;  //stmt(statement) untuk menyimpan query
+
+    public function __construct(){
+        // data source name
+        $dsn = 'mysql:host=localhost;dbname=phpmvc';
+
+        try{
+            // PDO(dsn, username, password);
+            $this->dbh = new PDO($dsn, 'root', '');
+        } catch(PDOException $e){
+            die($e->getMessage());
+        }
+    }
 
     public function getAllMahasiswa(){
-        return $this->mhs;
+
+        $this->stmt = $this->dbh->prepare('SELECT * FROM mahasiswa');
+        // menjalankan query
+        $this->stmt->execute();
+        // mengmabil semua data sebagai array assosiatif
+        return $this->stmt->fetchAll(PDO::FETCH_ASSOC);
     }
 }
